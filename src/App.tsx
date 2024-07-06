798a86f56a74f81fba0215ab586a8b161968f61c
Hirokazu Toyomori <hirokazu.toyomori@acesinc.co.jp>
2024-07-06 15:41:27 +0900

    コンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import FormComponent from './components/FormComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to My React App</h1>
+      <FormComponent />
     </div>
   );
 }
